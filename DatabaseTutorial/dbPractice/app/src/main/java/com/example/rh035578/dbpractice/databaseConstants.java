a741feeeb5adffdf6037919e70d57c4ee76ae843
dontTOUCHDOWNthere <rhudgeons88@gmail.com>
2015-07-16 11:50:13 -0500

    can add and delete from database

---
+++ DatabaseTutorial/dbPractice/app/src/main/java/com/example/rh035578/dbpractice/databaseConstants.java
@@ -0,0 +1,19 @@
+package com.example.rh035578.dbpractice;
+
+/**
+ * Created by rh035578 on 7/16/15.
+ */
+public class databaseConstants {
+    public databaseConstants() {
+        //empty constructor
+    }
+
+    public static abstract class myConstants {
+        public static final String NAME = "practiceDB";
+        public static final String TABLE = "practiceTable1";
+        public static final int VERSION = 1;
+        public static final String ID = "_id";
+        public static final String FOOD = "food";
+        public static final String PRICE = "price";
+    }
+}
